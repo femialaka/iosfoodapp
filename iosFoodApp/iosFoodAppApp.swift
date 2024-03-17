47f231ed13c069e430d11edd71f107ab43574c6b
Femi Alaka <femialakadev@gmail.com>
2024-03-17 14:46:11 +0000

    Initial Commit

---
+++ iosFoodApp/iosFoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosFoodAppApp.swift
+//  iosFoodApp
+//
+//  Created by Femi Alaka on 17/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct iosFoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
